efa595108c19c68ca27a2a46a8fe6482c0cce682
Dibo72 <humanida133@gmail.com>
2026-02-03 12:28:17 +0100

    Ejerciico mejorado

--- src/ContarLinea.java
+++ src/ContarLinea.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class ContarLinea {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int contador = 0;
+        System.out.println("Palabra a contar: Pastel");
+        try(BufferedReader lector = new BufferedReader(new FileReader("archivo.txt"))){
+            String linea;
+            System.out.println("Archivo:");
+            while ((linea = lector.readLine()) != null){
+                System.out.println(linea);
+                linea = linea.toLowerCase();
+                if (linea.contains("pastel")){
+                    contador ++;
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
+        System.out.println();
+        System.out.println("Numero de lineas que contienen la palabra: " + contador);
     }
 }
